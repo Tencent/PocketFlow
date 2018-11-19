8e1341090f3d679f4cf5bb7fbb7159badda2b5fd
Kranthi Kiran GV <kranthi.gv@gmail.com>
2018-11-19 17:54:48 +0530

    Fix naming and code conventions

--- utils/get_idle_gpus.py
+++ utils/get_idle_gpus.py
@@ -25,28 +25,28 @@ assert len(sys.argv) == 2
 nb_idle_gpus = int(sys.argv[1])
 
 # assume: idle gpu has no more than 50% of total card memory used
-maxGpuUsageThreshold = .5
+max_gpu_usage_threshold = .5
 
 # command to execute to get gpu id and corresponding memory used
 # and total memory. It gives output in the format
 # gpu id, memory used, total memory
 cmd = 'nvidia-smi --query-gpu=index,memory.used,memory.total ' \
-    '--format=csv,noheader,nounits'
-gpuSmiOutput = subprocess.check_output(cmd, shell=True)
-gpuSmiOutput = gpuSmiOutput.decode('utf-8')
+  '--format=csv,noheader,nounits'
+gpu_smi_output = subprocess.check_output(cmd, shell=True)
+gpu_smi_output = gpu_smi_output.decode('utf-8')
 
 idle_gpus = []
 
-for gpu in gpuSmiOutput.split(sep='\n')[:-1]:
-    (gpuId, used, total) = [int(value) for value in gpu.split(sep=',')]
-    memoryUsagePercentage = (used/total)
-    if memoryUsagePercentage < maxGpuUsageThreshold:
-        # GPU usage is less than the threshold
-        idle_gpus.append(gpuId)
+for gpu in gpu_smi_output.split(sep='\n')[:-1]:
+  (gpu_id, used, total) = [int(value) for value in gpu.split(sep=',')]
+  memory_usage_percentage = (used/total)
+  if memory_usage_percentage < max_gpu_usage_threshold:
+    # GPU usage is less than the threshold
+    idle_gpus.append(gpu_id)
 
 if len(idle_gpus) < nb_idle_gpus:
-    raise ValueError('not enough idle GPUs;'
-                     ' idle GPUs are: {}'.format(idle_gpus))
+  raise ValueError('not enough idle GPUs;'
+                   ' idle GPUs are: {}'.format(idle_gpus))
 idle_gpus = idle_gpus[:nb_idle_gpus]
 idle_gpus_str = ','.join([str(idle_gpu) for idle_gpu in idle_gpus])
 print(idle_gpus_str)
