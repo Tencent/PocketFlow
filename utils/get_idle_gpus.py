a417f3479a998e8f2f82c6dea2f731146a3c8a9d
jiaxiang-wu <jiaxiang.wu.90@gmail.com>
2019-02-13 09:45:38 +0800

    sort idle GPUs in the ascending order of mem usage

--- utils/get_idle_gpus.py
+++ utils/get_idle_gpus.py
@@ -14,7 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Get a list of idle GPUs."""
+"""Get a list of idle GPUs.
+
+This script sorts GPUs in the ascending order of memory usage, and return the top-k ones.
+"""
 
 import os
 import sys
@@ -25,7 +28,7 @@ assert len(sys.argv) == 2
 nb_idle_gpus = int(sys.argv[1])
 
 # assume: idle gpu has no more than 50% of total card memory used
-max_gpu_usage_threshold = .5
+mem_usage_ulimit = .5
 
 # command to execute to get gpu id and corresponding memory used
 # and total memory. It gives output in the format
@@ -36,17 +39,16 @@ gpu_smi_output = subprocess.check_output(cmd, shell=True)
 gpu_smi_output = gpu_smi_output.decode('utf-8')
 
 idle_gpus = []
-
 for gpu in gpu_smi_output.split(sep='\n')[:-1]:
-  (gpu_id, used, total) = [int(value) for value in gpu.split(sep=',')]
-  memory_usage_percentage = (used/total)
-  if memory_usage_percentage < max_gpu_usage_threshold:
-    # GPU usage is less than the threshold
-    idle_gpus.append(gpu_id)
+  (gpu_id, mem_used, mem_total) = [int(value) for value in gpu.split(sep=',')]
+  mem_usage = float(mem_used) / mem_total
+  if mem_usage < mem_usage_ulimit:
+    idle_gpus += [(gpu_id, mem_usage)]
+idle_gpus.sort(key=lambda x: x[1])
+idle_gpus = [x[0] for x in idle_gpus]  # only keep GPU ids
 
 if len(idle_gpus) < nb_idle_gpus:
-  raise ValueError('not enough idle GPUs;'
-                   ' idle GPUs are: {}'.format(idle_gpus))
+  raise ValueError('not enough idle GPUs; idle GPUs are: {}'.format(idle_gpus))
 idle_gpus = idle_gpus[:nb_idle_gpus]
 idle_gpus_str = ','.join([str(idle_gpu) for idle_gpu in idle_gpus])
 print(idle_gpus_str)
