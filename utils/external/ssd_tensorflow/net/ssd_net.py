dbe9ded615c117b1c51f2d65b93bf9d8d96664dd
hematank <hematank@github.com>
2019-03-08 14:21:04 +0800

    完善mobilenetv1ssd训练和压缩

--- utils/external/ssd_tensorflow/net/ssd_net.py
+++ utils/external/ssd_tensorflow/net/ssd_net.py
@@ -234,6 +234,213 @@ class VGG16Backbone(object):
                 )
             return conv_bn_blocks
 
+class Mobilenetv1Backbone(object):
+    def __init__(self, data_format='channels_first'):
+        super(Mobilenetv1Backbone, self).__init__()
+        self._data_format = data_format
+        self._bn_axis = -1 if data_format == 'channels_last' else 1
+
+        # initializer = tf.glorot_uniform_initializer  glorot_normal_initializer
+        self._conv_initializer = tf.glorot_uniform_initializer
+        self._conv_bn_initializer = tf.glorot_uniform_initializer  # lambda : tf.truncated_normal_initializer(mean=0.0, stddev=0.005)
+        # MobilenetV1 layers
+        self._conv0_block = self.conv_block(1, 32, 3, (2, 2), 'conv0')
+        self._conv1_dw_block = self.separable_conv_block(1, 32, 3, (1, 1), 'conv1_dw')
+        self._conv1_block = self.conv_block(1, 64, 1, (1, 1), 'conv1')
+        self._conv2_dw_block = self.separable_conv_block(1, 64, 3, (2, 2), 'conv2_dw')
+        self._conv2_block = self.conv_block(1, 128, 1, (1, 1), 'conv2')
+        self._conv3_dw_block = self.separable_conv_block(1, 128, 3, (1, 1), 'conv3_dw')
+        self._conv3_block = self.conv_block(1, 128, 1, (1, 1), 'conv3')
+        self._conv4_dw_block = self.separable_conv_block(1, 128, 3, (2, 2), 'conv4_dw')
+        self._conv4_block = self.conv_block(1, 256, 1, (1, 1), 'conv4')
+        self._conv5_dw_block = self.separable_conv_block(1, 256, 3, (1, 1), 'conv5_dw')
+        self._conv5_block = self.conv_block(1, 256, 1, (1, 1), 'conv5')
+        self._conv6_dw_block = self.separable_conv_block(1, 256, 3, (2, 2), 'conv6_dw')
+        self._conv6_block = self.conv_block(1, 512, 1, (1, 1), 'conv6')
+        self._conv7_dw_block = self.separable_conv_block(1, 512, 3, (1, 1), 'conv7_dw')
+        self._conv7_block = self.conv_block(1, 512, 1, (1, 1), 'conv7')
+        self._conv8_dw_block = self.separable_conv_block(1, 512, 3, (1, 1), 'conv8_dw')
+        self._conv8_block = self.conv_block(1, 512, 1, (1, 1), 'conv8')
+        self._conv9_dw_block = self.separable_conv_block(1, 512, 3, (1, 1), 'conv9_dw')
+        self._conv9_block = self.conv_block(1, 512, 1, (1, 1), 'conv9')
+        self._conv10_dw_block = self.separable_conv_block(1, 512, 3, (1, 1), 'conv10_dw')
+        self._conv10_block = self.conv_block(1, 512, 1, (1, 1), 'conv10')
+        self._conv11_dw_block = self.separable_conv_block(1, 512, 3, (1, 1), 'conv11_dw')
+        self._conv11_block = self.conv_block(1, 512, 1, (1, 1), 'conv11')
+        self._conv12_dw_block = self.separable_conv_block(1, 512, 3, (2, 2), 'conv12_dw')
+        self._conv12_block = self.conv_block(1, 1024, 1, (1, 1), 'conv12')
+        self._conv13_dw_block = self.separable_conv_block(1, 1024, 3, (1, 1), 'conv13_dw')
+        self._conv13_block = self.conv_block(1, 1024, 1, (1, 1), 'conv13')
+
+        # SSD layers
+        with tf.variable_scope('additional_layers') as scope:
+            self._conv14_block = self.ssd_conv_block(256, 2, 'conv14')
+            self._conv15_block = self.ssd_conv_block(128, 2, 'conv15')
+            self._conv16_block = self.ssd_conv_block(128, 2, 'conv16')
+            self._conv17_block = self.ssd_conv_block(128, 2, 'conv17')
+
+    def l2_normalize(self, x, name):
+        with tf.name_scope(name, "l2_normalize", [x]) as name:
+            axis = -1 if self._data_format == 'channels_last' else 1
+            square_sum = tf.reduce_sum(tf.square(x), axis, keep_dims=True)
+            x_inv_norm = tf.rsqrt(tf.maximum(square_sum, 1e-10))
+            return tf.multiply(x, x_inv_norm, name=name)
+
+    def forward(self, inputs, training=False):
+        # inputs should in BGR
+        feature_layers = []
+        # forward mobilenet layers
+        for conv in self._conv0_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv1_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv1_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv2_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv2_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv3_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv3_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv4_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv4_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv5_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv5_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv6_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv6_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv7_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv7_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv8_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv8_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv9_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv9_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv10_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv10_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv11_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv11_block:
+            inputs = forward_module(conv, inputs, training=training)
+        feature_layers.append(inputs)
+
+        for conv in self._conv12_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv12_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv13_dw_block:
+            inputs = forward_module(conv, inputs, training=training)
+        for conv in self._conv13_block:
+            inputs = forward_module(conv, inputs, training=training)
+        feature_layers.append(inputs)
+
+        # forward ssd layers
+        for layer in self._conv14_block:
+            inputs = forward_module(layer, inputs, training=training)
+        # conv14
+        feature_layers.append(inputs)
+        for layer in self._conv15_block:
+            inputs = forward_module(layer, inputs, training=training)
+        # conv15
+        feature_layers.append(inputs)
+        for layer in self._conv16_block:
+            inputs = forward_module(layer, inputs, training=training)
+        # conv16
+        feature_layers.append(inputs)
+        for layer in self._conv17_block:
+            inputs = forward_module(layer, inputs, training=training)
+        # conv17
+        feature_layers.append(inputs)
+
+        return feature_layers
+
+    def conv_block(self, num_blocks, filters, kernel_size, strides, name, reuse=None):
+        with tf.variable_scope(name):
+            conv_blocks = []
+            for ind in range(1, num_blocks + 1):
+                conv_blocks.append(
+                        tf.layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding='same',
+                            data_format=self._data_format, activation=tf.nn.relu, use_bias=True,
+                            kernel_initializer=self._conv_initializer(),
+                            bias_initializer=tf.zeros_initializer(),
+                            name='{}_{}'.format(name, ind), _scope='{}_{}'.format(name, ind), _reuse=None)
+                    )
+            return conv_blocks
+    def separable_conv_block(self, num_blocks, filters, kernel_size, strides, name, reuse=None):
+        with tf.variable_scope(name):
+            separable_conv = []
+            for ind in range(1, num_blocks + 1):
+                separable_conv.append(tf.layers.SeparableConv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding='same',
+                            data_format=self._data_format, activation=tf.nn.relu, use_bias=True,
+                            name='{}_{}'.format(name, ind), _scope='{}_{}'.format(name, ind), _reuse=None))
+            return separable_conv
+
+    def ssd_conv_block(self, filters, strides, name, padding='same', reuse=None):
+        with tf.variable_scope(name):
+            conv_blocks = []
+            conv_blocks.append(
+                    tf.layers.Conv2D(filters=filters, kernel_size=1, strides=1, padding=padding,
+                        data_format=self._data_format, activation=tf.nn.relu, use_bias=True,
+                        kernel_initializer=self._conv_initializer(),
+                        bias_initializer=tf.zeros_initializer(),
+                        name='{}_1'.format(name), _scope='{}_1'.format(name), _reuse=None)
+                )
+            conv_blocks.append(
+                    tf.layers.Conv2D(filters=filters * 2, kernel_size=3, strides=strides, padding=padding,
+                        data_format=self._data_format, activation=tf.nn.relu, use_bias=True,
+                        kernel_initializer=self._conv_initializer(),
+                        bias_initializer=tf.zeros_initializer(),
+                        name='{}_2'.format(name), _scope='{}_2'.format(name), _reuse=None)
+                )
+            return conv_blocks
+
+    def ssd_conv_bn_block(self, filters, strides, name, reuse=None):
+        with tf.variable_scope(name):
+            conv_bn_blocks = []
+            conv_bn_blocks.append(
+                    tf.layers.Conv2D(filters=filters, kernel_size=1, strides=1, padding='same',
+                        data_format=self._data_format, activation=None, use_bias=False,
+                        kernel_initializer=self._conv_bn_initializer(),
+                        bias_initializer=None,
+                        name='{}_1'.format(name), _scope='{}_1'.format(name), _reuse=None)
+                )
+            conv_bn_blocks.append(
+                    tf.layers.BatchNormalization(axis=self._bn_axis, momentum=BN_MOMENTUM, epsilon=BN_EPSILON, fused=USE_FUSED_BN,
+                        name='{}_bn1'.format(name), _scope='{}_bn1'.format(name), _reuse=None)
+                )
+            conv_bn_blocks.append(
+                    ReLuLayer('{}_relu1'.format(name), _scope='{}_relu1'.format(name), _reuse=None)
+                )
+            conv_bn_blocks.append(
+                    tf.layers.Conv2D(filters=filters * 2, kernel_size=3, strides=strides, padding='same',
+                        data_format=self._data_format, activation=None, use_bias=False,
+                        kernel_initializer=self._conv_bn_initializer(),
+                        bias_initializer=None,
+                        name='{}_2'.format(name), _scope='{}_2'.format(name), _reuse=None)
+                )
+            conv_bn_blocks.append(
+                    tf.layers.BatchNormalization(axis=self._bn_axis, momentum=BN_MOMENTUM, epsilon=BN_EPSILON, fused=USE_FUSED_BN,
+                        name='{}_bn2'.format(name), _scope='{}_bn2'.format(name), _reuse=None)
+                )
+            conv_bn_blocks.append(
+                    ReLuLayer('{}_relu2'.format(name), _scope='{}_relu2'.format(name), _reuse=None)
+                )
+            return conv_bn_blocks
+
 def multibox_head(feature_layers, num_classes, num_anchors_depth_per_layer, data_format='channels_first'):
     with tf.variable_scope('multibox_head'):
         cls_preds = []
@@ -249,7 +456,23 @@ def multibox_head(feature_layers, num_classes, num_anchors_depth_per_layer, data
                         padding='same', data_format=data_format, activation=None,
                         kernel_initializer=tf.glorot_uniform_initializer(),
                         bias_initializer=tf.zeros_initializer()))
+        return loc_preds, cls_preds
 
+def multibox_head_mobilenetssd(feature_layers, num_classes, num_anchors_depth_per_layer, data_format='channels_first'):
+    with tf.variable_scope('multibox_head'):
+        cls_preds = []
+        loc_preds = []
+        for ind, feat in enumerate(feature_layers):
+            loc_preds.append(tf.layers.conv2d(feat, num_anchors_depth_per_layer[ind] * 4, (1, 1), use_bias=True,
+                        name='loc_{}'.format(ind), strides=(1, 1),
+                        padding='same', data_format=data_format, activation=None,
+                        kernel_initializer=tf.glorot_uniform_initializer(),
+                        bias_initializer=tf.zeros_initializer()))
+            cls_preds.append(tf.layers.conv2d(feat, num_anchors_depth_per_layer[ind] * num_classes, (1, 1), use_bias=True,
+                        name='cls_{}'.format(ind), strides=(1, 1),
+                        padding='same', data_format=data_format, activation=None,
+                        kernel_initializer=tf.glorot_uniform_initializer(),
+                        bias_initializer=tf.zeros_initializer()))
         return loc_preds, cls_preds
 
 
